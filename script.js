72cb668c19edfe9cdea4bfd043ac4a61b5c361f8
bruno.santos <bruno.santos@sequor.com.br>
2023-12-01 12:57:33 -0300

    time implemented

--- script.js
+++ script.js
@@ -4,21 +4,54 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
+const imagemIniciarOuPausar = document.querySelector('.app__card-primary-butto-icon');
+const tempoNaTela = document.querySelector('#timer');
+
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const audioPlay = new Audio('/sons/play.wav');
+const audioPause = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('/sons/beep.mp3');
+
+let tempoDecorridoEmSegundos = 1500;
+let intervaloId = null;
+
+musica.loop = true;
+
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused)
+        musica.play();
+    else
+        musica.pause();
+})
 
 focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500;
     alterarContexto('foco');
+    focoBt.classList.add('active');
 });
 
-
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300;
     alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
 });
 
 longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900;
     alterarContexto('descanso-longo');
+    longoBt.classList.add('active');
 });
 
 function alterarContexto(contexto) {
+    mostrarTempo();
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove(active);
+    });
+
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
     switch (contexto){
@@ -42,3 +75,46 @@ function alterarContexto(contexto) {
         }
     }
 }
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        audioTempoFinalizado.play();
+        iniciarOuPausarBt.textContent = "Começar";
+        imagemIniciarOuPausar.setAttribute('src', '/imagens/play_arrow.png');
+        alert('Tempo finalizado!');
+        zerar();
+        tempoDecorridoEmSegundos = 5;
+        return;
+    }
+    tempoDecorridoEmSegundos -= 1;
+    mostrarTempo();
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar() {
+    if (intervaloId) {
+        audioPause.play();
+        zerar();
+        return;
+    }
+    audioPlay.play();
+    intervaloId = setInterval(contagemRegressiva, 1000);
+    iniciarOuPausarBt.textContent = "Pausar";
+    imagemIniciarOuPausar.setAttribute('src', '/imagens/pause.png');
+}
+
+function zerar() {
+    clearInterval(intervaloId);
+    iniciarOuPausarBt.textContent = "Começar";
+    imagemIniciarOuPausar.setAttribute('src', '/imagens/play_arrow.png');
+    intervaloId = null;
+}
+
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+    const tempoFormatado = tempo.toLocaleTimeString('pt-BR', {minute: '2-digit', second: '2-digit'});
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+
+mostrarTempo();
